5c41970eae83583a875a90f11bf8cc5efdb463e5
ivan.liu <xiaowen.liu@nxp.com>
2017-03-08 11:26:54 +0800

    MA-9306 move fsl added definition from ion.h to mxc_ion.h.
    move mxc_ion.h to uapi folder.
    clean fsl added definition from ion.h to mxc_ion.h.
    
    Change-Id: I4fa413243bdf9f9b5e0e76a3b370ed01a0e80aee
    Signed-off-by: ivan.liu <xiaowen.liu@nxp.com>

--- drivers/staging/android/uapi/mxc_ion.h
+++ drivers/staging/android/uapi/mxc_ion.h
@@ -2,7 +2,8 @@
  * drivers/gpu/mxc/mxc_ion.h
  *
  * Copyright (C) 2011 Google, Inc.
- * Copyright (C) 2012-2015 Freescale Semiconductor, Inc.
+ * Copyright (C) 2012-2016 Freescale Semiconductor, Inc.
+ * Copyright 2017 NXP.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -18,13 +19,32 @@
 #ifndef _LINUX_MXC_ION_H
 #define _LINUX_MXC_ION_H
 
-#include "../ion.h"
+#include <linux/ioctl.h>
+#include <linux/types.h>
+
+#define ION_CMA_HEAP_ID     0
 
 struct ion_phys_data {
 	ion_user_handle_t handle;
 	unsigned long phys;
 };
 
-#define ION_IOC_PHYS   _IOWR(ION_IOC_MAGIC, 7, struct ion_phys_data)
+struct ion_phys_dma_data {
+	unsigned long phys;
+	size_t size;
+	int dmafd;
+};
+
+struct ion_phys_virt_data {
+	unsigned long virt;
+	unsigned long phys;
+	size_t size;
+};
+
+#define ION_IOC_PHYS   _IOWR(ION_IOC_MAGIC, 8, struct ion_phys_data)
+
+#define ION_IOC_PHYS_DMA   _IOWR(ION_IOC_MAGIC, 9, struct ion_phys_dma_data)
+
+#define ION_IOC_PHYS_VIRT   _IOWR(ION_IOC_MAGIC, 10, struct ion_phys_virt_data)
 
 #endif
