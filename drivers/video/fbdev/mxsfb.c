167b1f430d2bb178d22f32f790d55b08352a6a87
Fancy Fang <chen.fang@nxp.com>
2017-04-10 13:50:51 +0800

    MLK-14655-1 video: mxsfb: don't clear AS regs when overlay fb release
    Don't clear the 'LCDC_AS_CTRL' and 'LCDC_AS_NEXT_BUF' registers
    in the function 'overlayfb_release()', since the next user may
    enable overlay fb without calling set_par first.
    
    Signed-off-by: Fancy Fang <chen.fang@nxp.com>

--- drivers/video/fbdev/mxsfb.c
+++ drivers/video/fbdev/mxsfb.c
@@ -1693,7 +1693,6 @@ static int overlayfb_open(struct fb_info *info, int user)
 static int overlayfb_release(struct fb_info *info, int user)
 {
 	struct mxsfb_layer *ofb = (struct mxsfb_layer*)info->par;
-	struct mxsfb_info *fbi = ofb->fbi;
 
 	BUG_ON(!atomic_read(&ofb->usage));
 
@@ -1702,8 +1701,6 @@ static int overlayfb_release(struct fb_info *info, int user)
 			ofb->ops->disable(ofb);
 
 		ofb->blank_state = -1;
-		writel(0x0, fbi->base + LCDC_AS_CTRL);
-		writel(0x0, fbi->base + LCDC_AS_NEXT_BUF);
 	}
 
 	return 0;
