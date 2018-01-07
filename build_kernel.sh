e6d891d950c61164ea9f4b2a0086c9af24ab425f
dpirly <dpirly@gmail.com>
2018-01-07 15:59:32 +0800

    create k6206 branch and push build_kernel.sh script

---
+++ build_kernel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#export CROSS_COMPILE=<path to cross compiler prefix> (e.g.,
+#/opt/poky/1.4.1/sysroots/i686-pokysdk-linux/usr/bin/cortexa9hf-vfp-neon-poky-linuxgnueabi/
+
+source /opt/fsl-imx-fb/4.1.15-2.1.0/environment-setup-cortexa7hf-neon-poky-linux-gnueabi
+
+
+export ARCH=arm
+export CROSS_COMPILER=arm-poky-linux-gnueabi-
+unset LDFLAGS
+
+make imx_v7_defconfig
+make -j8 uImage LOADADDR=0x10008000
